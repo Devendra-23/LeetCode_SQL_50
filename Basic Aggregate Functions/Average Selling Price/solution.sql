7821ab83df2e63b9183aa768dea7cf05060b4a17
Devendra-23 <dev6dhokare@gmail.com>
2025-02-20 17:14:03 +0530

    Day 11 SQL

--- Basic Aggregate Functions/Average Selling Price/solution.sql
+++ Basic Aggregate Functions/Average Selling Price/solution.sql
@@ -1,8 +1,8 @@
-SELECT 
-    Prices.product_id,
-    ROUND(SUM(Prices.price * UnitsSold.units) / NULLIF(SUM(UnitsSold.units), 0), 2) AS average_price
+# Write your MySQL query statement below
+SELECT Prices.product_id, 
+       IFNULL(ROUND(SUM(Prices.price * UnitsSold.units) / SUM(UnitsSold.units), 2), 0) AS average_price
 FROM Prices
-LEFT JOIN UnitsSold
+LEFT JOIN UnitsSold 
 ON Prices.product_id = UnitsSold.product_id 
 AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
 GROUP BY Prices.product_id;
